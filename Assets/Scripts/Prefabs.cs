187cff40a6840d1ba7fd84ad8318fba73e3ada18
Jan Rabe <jan.rabe@exozet.com>
2015-02-03 14:14:29 +0100

    added scene, detonator and a few image effects

---
+++ Assets/Scripts/Prefabs.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace WebcamExplosions
+{
+	public class Prefabs : MonoBehaviour
+	{
+
+		#region Singleton
+		
+		private static volatile Prefabs _instance;
+		private static readonly object SyncRoot = new Object ();
+		
+		public static Prefabs Shared {
+			get {
+				if (_instance == null) {
+					lock (SyncRoot) {
+						return _instance ?? (_instance = ((GameObject)Resources.Load ("Prefabs", typeof(GameObject))).GetComponent<Prefabs> ());
+					}
+				}
+				
+				return _instance;
+			}
+		}
+
+		#endregion
+
+		public GameObject Explosion;
+	}
+
+	public static class GameObjectExtensions
+	{
+		public static GameObject Instantiate (this GameObject prefab)
+		{
+			return GameObject.Instantiate (prefab) as GameObject;
+		}
+	}
+}
---
+++ Assets/Scripts/WebCam.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace WebcamExplosions
+{
+	public class WebCam : MonoBehaviour {
+
+		void Start () {
+			WebCamTexture webcamTexture = new WebCamTexture();
+			GetComponent<MeshRenderer>().material.mainTexture = webcamTexture;
+			webcamTexture.Play();
+		}
+
+		public void OnMouseDown() 
+		{
+			Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+
+
+			Prefabs.Shared.Explosion.Instantiate().transform.position = pos;
+		}
+	}
+}

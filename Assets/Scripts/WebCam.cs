a6235d872f92ddc7c3d81b3e3797de1faf94cda7
Jan Rabe <jan.rabe@exozet.com>
2015-02-03 14:30:33 +0100

    added request for permissions

--- Assets/Scripts/WebCam.cs
+++ Assets/Scripts/WebCam.cs
@@ -6,16 +6,23 @@ namespace WebcamExplosions
 	public class WebCam : MonoBehaviour {
 
 		void Start () {
-			WebCamTexture webcamTexture = new WebCamTexture();
-			GetComponent<MeshRenderer>().material.mainTexture = webcamTexture;
-			webcamTexture.Play();
+			StartCoroutine(RequestAuthorization());
+		}
+
+		IEnumerator RequestAuthorization() {
+			yield return Application.RequestUserAuthorization(UserAuthorization.WebCam );
+			if (Application.HasUserAuthorization(UserAuthorization.WebCam)) {
+				WebCamTexture webcamTexture = new WebCamTexture();
+				GetComponent<MeshRenderer>().material.mainTexture = webcamTexture;
+				webcamTexture.Play();
+			} else {
+				Debug.Log ("meh~");
+			}
 		}
 
 		public void OnMouseDown() 
 		{
 			Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
-
-
 			Prefabs.Shared.Explosion.Instantiate().transform.position = pos;
 		}
 	}
